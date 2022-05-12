7f616da504831eb50a74d703557c58d79cd3a792
cristhianalejo03 <cristhianalejocastillo03@gmail.com>
2022-05-12 17:30:25 +0000

    cristhian

--- esrepetitivas/ejerciciosr.java
+++ esrepetitivas/ejerciciosr.java
@@ -1,5 +1,7 @@
 package esrepetitivas;
 
+import java.util.Scanner;
+
 public class ejerciciosr {
     
     public static void imprimirWhileN1_10() {
@@ -20,14 +22,23 @@ public class ejerciciosr {
     }
     public static void imprimirforN1_20() {
         for (int numinit = 19 ;numinit <=20; numinit++) {
-            System.out.println(numinit);
+            System.out.println(numinit); 
+        }
+    }
+    public static void nombrevertical() {
+        Scanner it=new Scanner(System.in);
+        System.out.println("ingrese su nombre"); 
+        String nombre =it.next();
+        for (int i = 0; i < nombre.length(); i++) {
+             System.out.println(nombre.charAt(i));
 
-            
         }
     }
     public static void main(String[] args) {
-       System.out.println("imprime while:"); imprimirWhileN1_10();
+       /*System.out.println("imprime while:"); imprimirWhileN1_10();
        System.out.println("imprime do while:"); imptimirDowhileeN1_20();
-       System.out.println("imprime for;"); imprimirforN1_20();
+       System.out.println("imprime for;"); imprimirforN1_20();*/
+       
+       nombrevertical();
     }
 }
